b40fa7d1036d063c61adc6b2fae1d3df20cd95c9
Tristin <charismental@gmail.com>
2019-06-18 15:36:46 -0700

    Set up die component with value prop

---
+++ src/components/Die.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="die">
+      <div class="dot a" :class='[{"hidden" : value <= 3}]'></div>
+      <div class="b"></div>
+      <div class="dot c" :class='[{"hidden" : value === 1}]'></div>
+      <div class="dot d" :class='[{"hidden" : value !== 6}]'></div>
+      <div class="dot e" :class='[{"hidden" : value % 2 === 0}]'></div>
+      <div class="dot f" :class='[{"hidden" : value !== 6}]'></div>
+      <div class="dot g" :class='[{"hidden" : value === 1}]'></div>
+      <div class="h"></div>
+      <div class="dot i" :class='[{"hidden" : value <= 3}]'></div>
+    </div>
+</template>
+
+<script>
+export default {
+props: {
+    value : Number
+}
+}
+</script>
+
+<style scoped>
+.die {
+  position: relative;
+  height: 48px;
+  width: 48px;
+  background-color: #FFF;
+  border: 2px solid #000;
+  display: inline-grid;
+  padding-top: 3px;
+  padding-left: 3px;
+  grid-gap: 3px;
+  grid-template-columns: auto auto auto;
+  grid-template-rows: auto auto auto;
+  grid-template-areas: "a b c"
+                      "d e f"
+                      "g h i";
+}
+.dot {
+  height: 10px;
+  width: 10px;
+  background-color: #000;
+  border-radius: 50%;
+}
+.a {
+ grid-area: a;
+}
+.b {
+ grid-area: b;
+}
+.c {
+ grid-area: c;
+}
+.d {
+ grid-area: d;
+}
+.e {
+ grid-area: e;
+}
+.f {
+ grid-area: f;
+}
+.g {
+ grid-area: g;
+}
+.h {
+ grid-area: h;
+}
+.i {
+ grid-area: i;
+}
+.hidden {
+ opacity: 0;
+}
+</style>
