c0333bb14dc018cf53bc37a74730ec08a70e662a
Tristin <charismental@gmail.com>
2019-06-18 15:37:20 -0700

    componentized die

--- src/components/GameTable.vue
+++ src/components/GameTable.vue
@@ -1,75 +1,30 @@
 <template>
   <div class="game-table">
-    <div id="d-one" class="dice">
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
+    <div v-for="die in dice" :key="die.id">
+      <Die :value="die.value" />
     </div>
-    <div id="d-two" class="dice">
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-    </div>
-    <div id="d-three" class="dice">
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-    </div>
-    <div  id="d-four" class="dice">
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot" style="opacity:0;"></div>
-    </div>
-    <div id="d-five" class="dice">
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-      <div class="dot" style="opacity:0;"></div>
-      <div class="dot"></div>
-    </div>
+    <button class="btn" @click="rollDice">Roll Dice</button>
   </div>
 </template>
 
 <script>
 import { mapState, mapMutations } from 'vuex'
+import Die from './Die'
 
 export default {
   name: 'GameTable',
+  components: {
+    Die
+  },
   computed: {
     ...mapState([
       'gameRunning',
       'players',
       'dice',
       'rollNumber'
-    ]),
+    ])
+  },
+  methods: {
     ...mapMutations([
       'rollDice'
     ])
@@ -84,47 +39,7 @@ export default {
   flex-wrap: wrap;
   justify-content: space-around;
 }
-.dice {
-  position: relative;
-  height: 48px;
-  width: 48px;
-  background-color: #FFF;
-  border: 2px solid #000;
-  display: inline-grid;
-  padding-top: 3px;
-  padding-left: 3px;
-  grid-gap: 3px;
-  grid-template-columns: auto auto auto;
-  grid-template-rows: auto auto auto;
-  grid-template-areas: "a b c"
-                      "d e f"
-                      "g h i";
-}
-.dot {
-  height: 10px;
-  width: 10px;
-  background-color: #000;
-  border-radius: 50%;
-}
-.one {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-}
-.two {
-
-}
-.three {
-
-}
-.four {
-
-}
-.five {
-
-}
-.six {
-
+.btn {
+  cursor: pointer;
 }
 </style>
