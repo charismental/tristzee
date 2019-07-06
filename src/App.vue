88ebfe66edd2ae58d27a7f677c8cc07983bd986c
Tristin <charismental@gmail.com>
2019-07-05 20:34:20 -0700

    Unbroke the game...Yay!!

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <v-toolbar flat dark color="blue-grey darken-2">
         <v-toolbar-title>TRISTZEE</v-toolbar-title>
         <v-spacer></v-spacer>
-          <v-toolbar-title>{{ activePlayer.name }} - {{ grandTotal(activePlayer.id) }}</v-toolbar-title>
+          <v-toolbar-title v-if="activePlayer">{{ activePlayer.name }} - {{ grandTotal(activePlayer.id) }}</v-toolbar-title>
           <v-btn icon color="warning darken-2" @click="newGame"><v-icon>mdi-restart</v-icon></v-btn>
       </v-toolbar>
       <v-container>
